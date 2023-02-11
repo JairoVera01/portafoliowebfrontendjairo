a94a9ff34f2f27186e6017af1ed3edac01dfcd36
Frank Jairo Vera <fj.verachamorro@gmail.com>
2023-02-11 12:56:43 -0500

    asdasd

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,7 @@ const temaClaro = () =>{
     document.querySelector("body").setAttribute("data-bs-theme","light")
     document.querySelector("#dl-icon").setAttribute("class","bi bi-moon-fill")
 }
+//Cambio con boton
 
 const cambiarTema = () =>{
     document.querySelector("body").getAttribute("data-bs-theme") === "light" ? temaOscuro() : temaClaro();
